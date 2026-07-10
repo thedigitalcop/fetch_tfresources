2bfc6a1384d755b9620b2c62f6527c431fc25594
rajni rajput <samjmu85@gmail.com>
2026-07-10 16:41:03 +0530

    Storage & Container

--- main.tf
+++ main.tf
@@ -2,3 +2,21 @@ resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-rg"
   location = "West Europe"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "${var.prefix}storage"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "${var.prefix}-container"
+  storage_account_id    = azurerm_storage_account.example.id
+  container_access_type = "private"
+}
